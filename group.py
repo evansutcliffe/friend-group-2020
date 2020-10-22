b79d8b8555bb0f7e860b3478b2b3da176e336753
Evan Sutcliffe <30503110+evansutcliffe@users.noreply.github.com>
2020-10-22 11:20:08 +0100

    updated files

--- group.py
+++ group.py
@@ -2,4 +2,14 @@
 
 # Your code to go here...
 
-my_group =
+my_group = [
+    {"Name":"Jill","Age": 26,"Job":"Biologist","Connections":[]},
+    {"Name":"Zalika","Age": 28,"Job":"Artist","Connections":[]},
+    {"Name":"John","Age": 27,"Job":"Writer","Connections":[]},
+    {"Name":"Nash","Age": 34,"Job":"Chef","Connections":[]},
+    ]
+
+my_group[0]["Connections"] = [(my_group[2],"Patner")] # Jill and John
+my_group[0]["Connections"].append((my_group[1],"Friend")) # Jill and John
+
+
